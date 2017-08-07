3367c29fe275d147c37eaf8d6c44af9d476be7ce
hejack0207 <hejack0207@sina.com>
2017-08-07 15:56:08 +0800

    recursive call of complete function

--- jsonschema/completers.py
+++ jsonschema/completers.py
@@ -28,11 +28,21 @@ completers={
 }   
 
 def complete(schema, completions):
+    print schema
     if 'type' in schema:
         print schema['type']
         c = completers[schema['type']]
         candidates = c()
         print candidates
+        completions['values']=candidates
+        if schema['type'] == 'object':
+            if 'properties' in schema:
+                properties=schema['properties']
+                keys=properties.keys()
+                print keys
+                completions['keys']=keys
+                for key in keys:
+                    complete(properties[key],completions)
     else:
         print 'no type'
 
